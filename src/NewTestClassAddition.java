32d7cc888b297a62823ba6988b64973ad6ad6c52
Rehan Sharaf <rehansharaf2011@gmail.com>
2018-05-17 13:08:34 -0700

    changes in NewTestClassAddition

--- src/NewTestClassAddition.java
+++ src/NewTestClassAddition.java
@@ -5,6 +5,8 @@ public class NewTestClassAddition {
 	public static void main (String [] args) {
 		
 		System.out.println("This is our main class");
+		
+		System.out.println("This is the new line we enter");
 	}
 
 }
